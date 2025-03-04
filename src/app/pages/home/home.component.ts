fd74726909238d01911687d7f74daed03112a8ed
Valeron <valeryaleshka@gmail.com>
2025-03-04 12:45:14 -0400

    ANGUL-007: added images paths

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ContentContainerComponent } from '../../shared/components/content-container/content-container.component';
+import { NgOptimizedImage } from '@angular/common';
+import { NzIconDirective } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [ContentContainerComponent],
+  imports: [ContentContainerComponent, NgOptimizedImage, NzIconDirective],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.less',
